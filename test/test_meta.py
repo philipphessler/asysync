4af11ac797af5f17a70c0e5b07e14ae533d14a99
Philipp Heßler <philipp.hessler@gmail.com>
2019-06-07 23:21:14 +0200

    Init asysync tests
    Create asysync test directory containing tests checking that meta
    information is defined.

---
+++ test/test_meta.py
@@ -0,0 +1,14 @@
+import asysync
+
+
+def test___author___is_set():
+    assert hasattr(asysync, "__author__")
+
+def test___email___is_set():
+    assert hasattr(asysync, "__email__")
+
+def test___version___is_set():
+    assert hasattr(asysync, "__version__")
+
+def test___license___is_set():
+    assert hasattr(asysync, "__license__")
